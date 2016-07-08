f059ab0e11c2a68f19c727baa166d103a5a457bb
zhangyuhao <zhangyuhao1158@sina.com>
2016-07-08 14:29:18 +0800

    Initial Commit

---
+++ RuntimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeDemo
+//
+//  Created by zyh on 16/7/8.
+//  Copyright © 2016年 zyh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
